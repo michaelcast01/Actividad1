a0f2ac6fae05c2db565bd18c697d65189925737e
michaelcast01 <michael2003cc@gmail.com>
2025-02-18 17:05:52 -0500

    mensaje consola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("michaelcastillo");
+        System.out.println("edad 21 ");
+        System.out.println("me gusta ir al gym");
+
+
     }
 }

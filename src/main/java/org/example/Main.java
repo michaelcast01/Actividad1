1a97e31c2ed0f0f063f3d54e64b48d81b5cc008c
michaelcast01 <michael2003cc@gmail.com>
2025-02-18 17:32:18 -0500

    agregar comandos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,21 @@ public class Main {
         System.out.println("michaelcastillo");
         System.out.println("edad 21 ");
         System.out.println("me gusta ir al gym");
+        System.out.println("Git config --global user.email “michael2003cc@gmail.com” ");
+        System.out.println("Git config –-list ");
+        System.out.println("Git status");
+        System.out.println("Git Branch ");
+        System.out.println("Git switch -c ");
+        System.out.println("Git switch ");
+        System.out.println("me gusta ir al gym");
+        System.out.println("me gusta ir al gym");
+        System.out.println("me gusta ir al gym");
+        System.out.println("me gusta ir al gym");
+        System.out.println("me gusta ir al gym");
+        System.out.println("me gusta ir al gym");
+        System.out.println("me gusta ir al gym");
+        System.out.println("me gusta ir al gym");
+
 
 
     }

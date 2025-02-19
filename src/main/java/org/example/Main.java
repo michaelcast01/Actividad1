32ba533fc0e8bfc4ddbbfda2a56fcf261eb7f7d1
michaelcast01 <michael2003cc@gmail.com>
2025-02-19 16:04:51 -0500

    final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("michaelcastillo");
+        System.out.println("edad 21 ");
+        System.out.println("me gusta ir al gym");
+        System.out.println("Git config --global user.email “michael2003cc@gmail.com” ");
+        System.out.println("Git config –-list ");
+        System.out.println("Git status");
+        System.out.println("Git Branch ");
+        System.out.println("Git switch -c ");
+        System.out.println("Git switch ");
+        System.out.println("Git add .");
+        System.out.println("Git log");
+        System.out.println("Git pull");
+        System.out.println("Git push");
+        System.out.println("Git commit -m ");
+        System.out.println("Git branch -d elimina rama de forma local");
+        System.out.println("Git push origin --delete elimina la rama de forma remota");
 
     }
 }
